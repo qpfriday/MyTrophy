5f5c544a6b33d3d9c0fd4cac8f02af0876420da6
chan <qpfriday@gmail.>
2024-05-19 11:01:33 +0900

    프로젝트 생성

---
+++ src/main/java/project3/mytrophy/MytrophyApplication.java
@@ -0,0 +1,13 @@
+package project3.mytrophy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytrophyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MytrophyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project3/mytrophy/MytrophyApplicationTests.java
@@ -0,0 +1,13 @@
+package project3.mytrophy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytrophyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
