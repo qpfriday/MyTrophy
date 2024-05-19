65fcd832fa4538aaa1a7c93f77ef22224c70aca5
chan <qpfriday@gmail.>
2024-05-19 15:27:49 +0900

    프로젝트 생성

--- src/main/java/mytrophy/MyTrophyApplication.java
+++ src/main/java/mytrophy/MyTrophyApplication.java
@@ -1,13 +1,13 @@
-package project3.mytrophy;
+package mytrophy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MytrophyApplication {
+public class MyTrophyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MytrophyApplication.class, args);
+        SpringApplication.run(MyTrophyApplication.class, args);
     }
 
 }
