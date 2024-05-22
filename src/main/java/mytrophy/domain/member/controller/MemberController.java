9cf2c3c1e46819ba6ca9840f3221759b285f39d2
chan <qpfriday@gmail.>
2024-05-22 20:09:41 +0900

    Feat: 회원가입 상태코드 추가

--- src/main/java/mytrophy/domain/member/controller/MemberController.java
+++ src/main/java/mytrophy/domain/member/controller/MemberController.java
@@ -2,21 +2,26 @@ package mytrophy.domain.member.controller;
 
 import mytrophy.domain.member.dto.SignupDto;
 import mytrophy.domain.member.service.SignupService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class SignupController {
+@RequestMapping("/members")
+public class MemberController {
     private final SignupService signupService;
 
-    public SignupController(SignupService signupService) {
+    public MemberController(SignupService signupService) {
         this.signupService = signupService;
     }
 
     @PostMapping("/signup")
-    public String signupProcess(SignupDto signupDto) {
+    public ResponseEntity<String> signupProcess(SignupDto signupDto) {
         signupService.SignupProcess(signupDto);
 
-        return "ok";
+
+        return new ResponseEntity<>("Signup successful", HttpStatus.CREATED);
     }
 }
