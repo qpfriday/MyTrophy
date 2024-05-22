565556bdf01f4cc2ef2f4959d1a42371570cccd9
soonduck <soonduck3729@gmail.com>
2024-05-22 16:07:04 +0900

    feat: 말머리 별 게시글 조회 메서드 추가

--- src/main/java/mytrophy/api/article/repository/ArticleRepository.java
+++ src/main/java/mytrophy/api/article/repository/ArticleRepository.java
@@ -1,9 +1,16 @@
 package mytrophy.api.article.repository;
 
 import mytrophy.api.article.domain.Article;
+import mytrophy.api.article.domain.Header;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+    List<Article> findByHeader(Header header);
+
+    Article findByIdAndHeader(Long id, Header header);
 }
