08161f3c064472482e7ec6473c7e82251577198d
jogyueun <55841366+jogyueun@users.noreply.github.com>
2024-05-22 14:27:33 +0900

    Game 관련 Repository 생성

--- src/main/java/mytrophy/api/game/entity/Games.java
+++ src/main/java/mytrophy/api/game/entity/Games.java
@@ -29,7 +29,7 @@ public class Games {
     private String shortcutDescription;
 
     @Column
-    private String explain;
+    private String explanation;
 
     @Column
     private String developers;
