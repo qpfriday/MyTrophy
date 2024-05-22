25b8dd7b92643f7919af53ae056cc53388a660e8
soonduck <soonduck3729@gmail.com>
2024-05-22 13:07:49 +0900

    feat: article entity 작성

---
+++ src/main/java/mytrophy/api/article/domain/Article.java
@@ -0,0 +1,60 @@
+package mytrophy.api.article.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 생성자를 protected로 설정
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 자동 생성되는 값으로 설정
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private Header header;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String content;
+
+
+    private int cntUp; // 좋아요 수
+
+
+    @Builder // 빌더 패턴 적용
+    public Article(Header header, String name, String content, int cntUp) {
+        this.header = header;
+        this.name = name;
+        this.content = content;
+        this.cntUp = cntUp;
+    }
+
+    // 게시글 생성 로직
+    public static Article createArticle(Header header, String name, String content) {
+        return Article.builder()
+            .header(header)
+            .name(name)
+            .content(content)
+            .cntUp(0)
+            .build();
+    }
+
+    // 게시글 수정
+    public void update(Header header, String name, String content) {
+        this.header = header;
+        this.name = name;
+        this.content = content;
+    }
+
+    // 좋아요 증가
+    public void upCntUp() {
+        this.cntUp++;
+    }
+}
