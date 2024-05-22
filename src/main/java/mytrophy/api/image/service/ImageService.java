30e1d57c8b1a1eac2634c04ec0d9c4f269587ce0
soonduck <soonduck3729@gmail.com>
2024-05-22 20:18:13 +0900

    feat: image service 추가

---
+++ src/main/java/mytrophy/api/image/service/ImageService.java
@@ -0,0 +1,16 @@
+package mytrophy.api.image.service;
+
+import mytrophy.api.image.repository.ImageRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImageService {
+
+    private ImageRepository imageRepository;
+
+    // 이미지 불러오기
+    public void loadImage() {
+
+
+    }
+}
