d8114e9cd9ae5818cd8e07c67063b67cf16f9c0e
soonduck <soonduck3729@gmail.com>
2024-05-24 22:32:39 +0900

    remove: 사용하지 않는 import 삭제

--- src/main/java/mytrophy/api/config/FirebaseConfig.java
+++ src/main/java/mytrophy/api/config/FirebaseConfig.java
@@ -9,11 +9,9 @@ import com.google.firebase.cloud.StorageClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
+
 
 @Configuration
 public class FirebaseConfig {
