60ef17d08c622485b6ebbc76a77b7e8d87d583b1
soonduck <soonduck3729@gmail.com>
2024-05-24 21:33:34 +0900

    feat: 메서드 추가

--- src/main/java/mytrophy/api/article/repository/ArticleRepository.java
+++ src/main/java/mytrophy/api/article/repository/ArticleRepository.java
@@ -13,4 +13,6 @@ public interface ArticleRepository extends JpaRepository<Article, Long> {
     List<Article> findByHeader(Header header);
 
     Article findByIdAndHeader(Long id, Header header);
+
+    List<String> deleteByImagePath(String imagePath);
 }
