2476a2129c2d4930340becbc83573b94d242d5b7
soonduck <soonduck3729@gmail.com>
2024-05-27 16:10:27 +0900

    Refactor: 코드 리펙토링

--- src/main/java/mytrophy/api/article/dto/ArticleRequest.java
+++ src/main/java/mytrophy/api/article/dto/ArticleRequest.java
@@ -1,5 +1,6 @@
 package mytrophy.api.article.dto;
 
+import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
 import mytrophy.api.article.enumentity.Header;
@@ -11,6 +12,10 @@ public class ArticleRequest {
     private String content;
     private String imagePath;
 
+    public ArticleRequest() {
+    }
+
+    @Builder
     public ArticleRequest(Header header, String name, String content) {
         this.header = header;
         this.name = name;
