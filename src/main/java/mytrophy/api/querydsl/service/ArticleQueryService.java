0615227ae0fd31c0e0dc072e68b223250aac1097
soonduck <soonduck3729@gmail.com>
2024-05-28 17:21:35 +0900

    feat: QueryDSL service 생성

---
+++ src/main/java/mytrophy/api/querydsl/service/ArticleQueryService.java
@@ -0,0 +1,10 @@
+package mytrophy.api.querydsl.service;
+
+import mytrophy.api.article.entity.Article;
+
+import java.util.List;
+
+public interface ArticleQueryService {
+
+    List<Article> findArticleWithCommentsOrderedByLatest(Long articleId);
+}
---
+++ src/main/java/mytrophy/api/querydsl/service/ArticleQueryServiceImpl.java
@@ -0,0 +1,20 @@
+package mytrophy.api.querydsl.service;
+
+import lombok.RequiredArgsConstructor;
+import mytrophy.api.article.entity.Article;
+import mytrophy.api.querydsl.repository.ArticleQueryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ArticleQueryServiceImpl implements ArticleQueryService{
+
+    private final ArticleQueryRepository articleQueryRepository;
+
+    // 해당 게시글 조회 시 댓글 작성일자를 기준으로 내림차순 정렬
+    public List<Article> findArticleWithCommentsOrderedByLatest(Long articleId) {
+        return articleQueryRepository.findArticleWithCommentsOrderedByLatest(articleId);
+    }
+}
