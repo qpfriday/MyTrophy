26a158412e4d73de5761d1e79c8f1fe676c48c17
soonduck <soonduck3729@gmail.com>
2024-05-29 17:05:36 +0900

    feat: memberId 추가

--- src/main/java/mytrophy/api/article/dto/ArticleResponseDto.java
+++ src/main/java/mytrophy/api/article/dto/ArticleResponseDto.java
@@ -1,23 +1,24 @@
 package mytrophy.api.article.dto;
 
 import lombok.Getter;
+import mytrophy.api.article.entity.Article;
 import mytrophy.api.article.enumentity.Header;
+import mytrophy.api.member.entity.Member;
 
 @Getter
 public class ArticleResponseDto {
-    private Long id;
     private Header header;
     private String name;
     private String content;
     private String imagePath;
+    private Long memberId;
 
-
-    public ArticleResponseDto(Long id, Header header, String name, String content, String imagePath) {
-        this.id = id;
-        this.header = header;
-        this.name = name;
-        this.content = content;
-        this.imagePath = imagePath;
+    public ArticleResponseDto(Article article) {
+        this.header = article.getHeader();
+        this.name = article.getName();
+        this.content = article.getContent();
+        this.imagePath = article.getImagePath();
+        this.memberId = article.getMember().getId();
     }
 
 }
