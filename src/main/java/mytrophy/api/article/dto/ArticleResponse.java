a00b3e1bb20fa363fad7b13f35dbfe1b6f3e7e33
soonduck <soonduck3729@gmail.com>
2024-05-29 13:42:06 +0900

    Refactor: memberId 추가

--- src/main/java/mytrophy/api/article/dto/ArticleResponse.java
+++ src/main/java/mytrophy/api/article/dto/ArticleResponse.java
@@ -1,13 +1,16 @@
 package mytrophy.api.article.dto;
 
+import lombok.Getter;
 import mytrophy.api.article.enumentity.Header;
 
+@Getter
 public class ArticleResponse {
     private Long id;
     private Header header;
     private String name;
     private String content;
     private String imagePath;
+    private Long memberId;
 
     public ArticleResponse(Long id, Header header, String name, String content, String imagePath) {
         this.id = id;
