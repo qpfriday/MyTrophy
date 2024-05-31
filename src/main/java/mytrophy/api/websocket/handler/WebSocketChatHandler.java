e800a25dcef6d2cdfef10be92fec7def21f0de4a
soonduck <soonduck3729@gmail.com>
2024-05-31 20:41:20 +0900

    feat: WebSocket 추가

--- src/main/java/mytrophy/api/websocket/handler/WebSocketChatHandler.java
+++ src/main/java/mytrophy/api/websocket/handler/WebSocketChatHandler.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 @Slf4j
@@ -17,4 +19,8 @@ public class WebSocketChatHandler extends TextWebSocketHandler {
     private final ObjectMapper mapper;
 
     private final Set<WebSocketSession> sessions = new HashSet<>();
+
+    private final Map<Long, Set<WebSocketSession>> chatRoomSessionMap = new HashMap<>();
+
+
 }
