26d38576a70884fe621e846ef35fb60079967b74
soonduck <soonduck3729@gmail.com>
2024-06-02 19:18:41 +0900

    feat: Constants 클래스 추가

---
+++ src/main/java/mytrophy/global/constant/Constants.java
@@ -0,0 +1,8 @@
+package mytrophy.global.constant;
+
+public class Constants {
+    private static final String APP_ID = "appId";
+    private static final String CNT_UP = "cntUp";
+    private static final String CONTENT = "content";
+    private static final String MEMBER = "member";
+}
