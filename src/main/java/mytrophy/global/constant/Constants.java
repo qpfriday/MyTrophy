fc3c9286bcc99cf840baa88b8321bc0c195e53f7
soonduck <soonduck3729@gmail.com>
2024-06-03 14:26:54 +0900

    feat: Constants 상수 추가

--- src/main/java/mytrophy/global/constant/Constants.java
+++ src/main/java/mytrophy/global/constant/Constants.java
@@ -1,8 +1,90 @@
 package mytrophy.global.constant;
 
 public class Constants {
-    private static final String APP_ID = "appId";
-    private static final String CNT_UP = "cntUp";
-    private static final String CONTENT = "content";
-    private static final String MEMBER = "member";
+    // QArticle 클래스 관련 상수
+    public static final String COLUMN_APP_ID = "appId";
+    public static final String COLUMN_CNT_UP = "cntUp";
+    public static final String COLUMN_COMMENT_COUNT = "commentCount";
+    public static final String COLUMN_CONTENT = "content";
+    public static final String COLUMN_CREATED_AT = "createdAt";
+    public static final String COLUMN_HEADER = "header";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_IMAGE_PATH = "imagePath";
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_UPDATED_AT = "updatedAt";
+
+    // QArticleLike 클래스 관련 상수
+    public static final String COLUMN_ARTICLE = "article";
+    public static final String COLUMN_MEMBER = "member";
+
+    // QComment 관련 상수
+    public static final String COLUMN_CHILDREN_COMMENT = "childrenComment";
+    public static final String COLUMN_LIKES = "likes";
+    public static final String COLUMN_PARENT_COMMENT = "parentComment";
+
+    // QCommentLike 클래스 관련 상수
+    public static final String COLUMN_COMMENT = "comment";
+
+    // QBaseEntity 관련 상수
+    public static final String COLUMN_CREATED_AT_BASE = "createdAt";
+    public static final String COLUMN_ID_BASE = "id";
+    public static final String COLUMN_UPDATED_AT_BASE = "updatedAt";
+
+    // QAchievement 관련 상수
+    public static final String COLUMN_IMAGE_PATH_ACHIEVEMENT = "imagePath";
+    public static final String COLUMN_NAME_ACHIEVEMENT = "name";
+
+    // QCategory 관련 상수
+    public static final String COLUMN_GAME_CATEGORY_LIST = "gameCategoryList";
+    public static final String COLUMN_NAME_CATEGORY = "name";
+
+    // QGame 관련 상수
+    public static final String COLUMN_ACHIEVEMENT_LIST = "achievementList";
+    public static final String COLUMN_APP_ID_GAME = "appId";
+    public static final String COLUMN_DESCRIPTION = "description";
+    public static final String COLUMN_DEVELOPER = "developer";
+    public static final String COLUMN_EN_IS_POSSIBLE = "enIsPosible";
+    public static final String COLUMN_HEADER_IMAGE_PATH = "headerImagePath";
+    public static final String COLUMN_JP_IS_POSSIBLE = "jpIsPosible";
+    public static final String COLUMN_KO_IS_POSSIBLE = "koIsPosible";
+    public static final String COLUMN_PRICE = "price";
+    public static final String COLUMN_PUBLISHER = "publisher";
+    public static final String COLUMN_RECOMMENDATION = "recommendation";
+    public static final String COLUMN_RELEASE_DATE = "releaseDate";
+    public static final String COLUMN_REQUIREMENT = "requirement";
+    public static final String COLUMN_SCREENSHOT_LIST = "screenshotList";
+
+    // QGameData 관련 상수
+    public static final String COLUMN_APP_ID_GAME_DATA = "appId";
+
+    // QGameRead 관련 상수
+    public static final String COLUMN_LAST_APP_ID = "lastAppId";
+
+    // QScreenshot 관련 상수
+    public static final String COLUMN_FULL_IMAGE_PATH = "fullImagePath";
+    public static final String COLUMN_THUMBNAIL_IMAGE_PATH = "thumbnailImagePath";
+
+    // QImage 관련 상수
+    public static final String COLUMN_IMAGE_PATH_IMAGE = "imagePath";
+    public static final String COLUMN_NAME_IMAGE = "name";
+
+    // QMember 관련 상수
+    public static final String COLUMN_ARTICLES = "articles";
+    public static final String COLUMN_CATEGORIES = "categories";
+    public static final String COLUMN_CREATED_AT_MEMBER = "created_at";
+    public static final String COLUMN_EMAIL = "email";
+    public static final String COLUMN_IMAGE_PATH_MEMBER = "imagePath";
+    public static final String COLUMN_LOGIN_TYPE = "loginType";
+    public static final String COLUMN_NAME_MEMBER = "name";
+    public static final String COLUMN_NICKNAME = "nickname";
+    public static final String COLUMN_PASSWORD = "password";
+    public static final String COLUMN_ROLE = "role";
+    public static final String COLUMN_STEAM_ID = "steamId";
+    public static final String COLUMN_UPDATE_AT_MEMBER = "update_at";
+    public static final String COLUMN_USERNAME = "username";
+
+    // QMemberGame 관련 상수
+    public static final String COLUMN_APP_ID_MEMBER_GAME = "appId";
+    public static final String COLUMN_PLAYTIME_FOREVER = "playtimeForever";
+
 }
