c13f5f117ceb56a158d87080a18a1ab35943c81c
chan <qpfriday@gmail.>
2024-06-09 20:13:56 +0900

    Fix: Cors 수정

--- src/main/java/mytrophy/global/config/CorsMvcConfig.java
+++ src/main/java/mytrophy/global/config/CorsMvcConfig.java
@@ -11,7 +11,7 @@ public class CorsMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/**")                  // 다음 경로로 들어오는 요청에 대해
-                .allowedOrigins("http://localhost:3000") // 다음 경로에서 오는 요청을 허용 하며 (현재는 전부)
+                .allowedOrigins("http://localhost:3000", "http://34.64.52.132:3000") // 다음 경로에서 오는 요청을 허용 하며 (현재는 전부)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")   // 허용되는 메서드
                 .allowedHeaders("Authorization", "Content-Type")  // 허용되는 헤드
                 .exposedHeaders("Authorization", "Content-Type")  // 클라이언트측 응답에서 노출되는 헤더를 지정합니다.
