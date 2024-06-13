2506c0832b6859582baa82304df07368ba0d527b
juyeon <evek323@gmail.com>
2024-06-14 08:41:47 +0900

    Feat: swagger 설명 작성

--- src/main/java/mytrophy/api/game/controller/GameReviewController.java
+++ src/main/java/mytrophy/api/game/controller/GameReviewController.java
@@ -1,5 +1,7 @@
 package mytrophy.api.game.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import mytrophy.api.game.dto.RequestDTO;
 import mytrophy.api.game.dto.ResponseDTO;
@@ -28,10 +30,11 @@ public class GameReviewController {
     private final GameRecommendService gameRecommendService;
 
     //평가남기기 및 업데이트
+    @Operation(summary = "게임 평가 등록", description = "특정 게임에 대한 평가를 갱신합니다. (\"BAD\", \"GOOD\", \"PERFECT\")")
     @PostMapping("/{appId}/reviews")
-    public ResponseEntity<ResponseDTO.GetGameReviewDto> createOrUpdateReview(@PathVariable("appId") Integer appId,
-                                                                     @RequestBody RequestDTO.UpdateGameReviewDto dto,
-                                                                     @AuthenticationPrincipal CustomUserDetails userinfo) {
+    public ResponseEntity<ResponseDTO.GetGameReviewDto> createOrUpdateReview(@Parameter(description = "평가를 남기고자 하는 게임의 APP_ID", required = true) @PathVariable("appId") Integer appId,
+                                                                             @RequestBody RequestDTO.UpdateGameReviewDto dto,
+                                                                             @AuthenticationPrincipal CustomUserDetails userinfo) {
 
         String username = userinfo.getUsername();
         Long memberId = memberRepository.findByUsername(username).getId();
@@ -41,14 +44,16 @@ public class GameReviewController {
     }
 
     //특정 게임에 대한 모든 평가들 조회하기
+    @Operation(summary = "특정 게임의 모든 평가 조회", description = "특정 게임에 남겨진 모든 평가를 조회합니다.")
     @GetMapping("/{appId}/reviews")
-    public ResponseEntity<List<ResponseDTO.GetGameReviewsDto>> getReviewsByAppId(@PathVariable("appId") Integer appId) {
+    public ResponseEntity<List<ResponseDTO.GetGameReviewsDto>> getReviewsByAppId(@Parameter(description = "조회하고자 하는 게임의 APP_ID", required = true) @PathVariable("appId") Integer appId) {
         List<ResponseDTO.GetGameReviewsDto> reviews = gameReviewService.getReviewsByAppId(appId);
         return ResponseEntity.ok(reviews);
     }
 
     //내가 평가남긴 게임 조회하기
-    @GetMapping("/myreviews")
+    @Operation(summary = "본인이 평가한 게임목록 조회", description = "본인이 평가한 게임의 목록을 조회합니다.")
+    @GetMapping("/my-reviews")
     public ResponseEntity<List<ResponseDTO.GetGameReviewDto>> getReviewsByMemberId(@AuthenticationPrincipal CustomUserDetails userinfo) {
 
         String username = userinfo.getUsername();
@@ -59,7 +64,8 @@ public class GameReviewController {
     }
 
     //내가 추천한 게임 조회하기
-    @GetMapping("reviews/myrecommended")
+    @Operation(summary = "본인이 좋은 평가를 남긴 게임목록 조회", description = "본인이 좋은 평가를 남긴 게임목록을 조회합니다 (reviewStatus = \"PERFECT\")")
+    @GetMapping("reviews/my-recommended")
     public ResponseEntity<List<ResponseDTO.GetGameReviewDto>> getRecommendedGames(@AuthenticationPrincipal CustomUserDetails userinfo) {
         String username = userinfo.getUsername();
         Long memberId = memberRepository.findByUsername(username).getId();
@@ -69,8 +75,9 @@ public class GameReviewController {
     }
 
     //특정게임에 내가 남긴 리뷰
-    @GetMapping("/{appId}/myreview")
-    public ResponseEntity<ResponseDTO.GetGameReviewsDto> getMyReviewByAppId(@PathVariable("appId") Integer appId,
+    @Operation(summary = "특정 게임에 본인이 남긴 평가 조회", description = "특정 게임에 본인이 남긴 평가를 조회합니다.")
+    @GetMapping("/{appId}/my-review")
+    public ResponseEntity<ResponseDTO.GetGameReviewsDto> getMyReviewByAppId(@Parameter(description = "조회하고자 하는 게임의 APP_ID", required = true) @PathVariable("appId") Integer appId,
                                                                             @AuthenticationPrincipal CustomUserDetails userinfo) {
         String username = userinfo.getUsername();
         Long memberId = memberRepository.findByUsername(username).getId();
@@ -79,10 +86,11 @@ public class GameReviewController {
         return ResponseEntity.ok(reviewDto);
     }
 
+    @Operation(summary = "맞춤형 추천게임 조회", description = "회원의 관심 카테고리, 게임 평가를 기반으로 추천된 게임을 조회합니다.")
     @GetMapping("/recommendations")
     public Page<ResponseDTO.GetGameDetailDTO> getRecommendations(@AuthenticationPrincipal CustomUserDetails userinfo,
-                                                                 @RequestParam(defaultValue = "0") int page,
-                                                                 @RequestParam(defaultValue = "10") int size) {
+                                                                 @Parameter(description = "페이지 번호") @RequestParam(defaultValue = "0") int page,
+                                                                 @Parameter(description = " 한 페이지의 데이터 개수") int size) {
         String username = userinfo.getUsername();
         Long memberId = memberRepository.findByUsername(username).getId();
 
