bdf357211f05d0c0e190390369b743fcb922fb0d
chan <qpfriday@gmail.>
2024-06-15 14:52:38 +0900

    Fix: url 경로 수정

--- src/main/java/mytrophy/api/member/controller/MemberController.java
+++ src/main/java/mytrophy/api/member/controller/MemberController.java
@@ -194,13 +194,13 @@ public class MemberController {
             try {
                 String steamUserId = steamService.validateLoginParameters(dto);
                 memberService.linkSteamAccount(currentUsername, steamUserId);
-                response.sendRedirect("http://34.64.52.132");
+                response.sendRedirect("http://mytrophy.site");
                 return new ResponseEntity<>("스팀 연동 성공", HttpStatus.OK);
 
             } catch (Exception e) {
                 e.printStackTrace();
                 try {
-                    response.sendRedirect("http://34.64.52.132?error=steam-link-failed");
+                    response.sendRedirect("http://mytrophy.site?error=steam-link-failed");
                 } catch (IOException ioException) {
                     ioException.printStackTrace();
                 }
@@ -220,7 +220,7 @@ public class MemberController {
 
             } catch (Exception e) {
                 e.printStackTrace();
-                response.sendRedirect("http://34.64.52.132?error=steam-link-failed");
+                response.sendRedirect("http://mytrophy.site?error=steam-link-failed");
                 return new ResponseEntity<>("스팀 로그인 실패", HttpStatus.BAD_REQUEST);
             }
         }
