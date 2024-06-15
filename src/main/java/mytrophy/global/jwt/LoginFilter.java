a05a488b652c32b4d75c5a3164da581b8228e303
soonduck <soonduck3729@gmail.com>
2024-06-15 14:53:49 +0900

    fix: access 토큰 시간 수정

--- src/main/java/mytrophy/global/jwt/LoginFilter.java
+++ src/main/java/mytrophy/global/jwt/LoginFilter.java
@@ -70,7 +70,7 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
         String role = auth.getAuthority();
 
         //토큰 생성
-        String access = jwtUtil.createJwt("access", username, role, 600000L);
+        String access = jwtUtil.createJwt("access", username, role, 60000000L);
         String refresh = jwtUtil.createJwt("refresh", username, role, 86400000L);
 
         //Refresh 토큰 저장
